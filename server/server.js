fb665068b15d3ac6fa46b55eea7e7f4d940a22da
HariPrasad R A <165310751+Hariprasad1812@users.noreply.github.com>
2026-09-14 18:45:36 +0530

    Refactor server.js for improved structure and clarity

--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { GoogleGenAI } from "@google/genai";
-
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
